584de6905e7c3e647d425a7a9ac506d7b4abc99c
djcerdas <d2words@gmail.com>
2017-06-20 11:09:17 -0600

    Update ovmDBCheck.sh

--- ovmDBCheck.sh
+++ ovmDBCheck.sh
@@ -10,15 +10,16 @@
 adminLog=`find /u01 -name AdminServer.log`
 echo $adminLog
 printf "\nChecking the backups:\n----------------------\n"
-printf "\nLast good backups:\n"
+printf "Last good backups:\n"
 grep backup $adminLog|grep complete|tail -5
 printf "\n----------------------\n"
-printf "\nInconsistencies:\n"
+printf "Inconsistencies:\n"
 grep backup $adminLog|egrep inconsistencies|tail -5
-printf "\nDB issues:\n"
+printf "\n----------------------\n"
+printf "DB issues:\n"
 printf "\nFirst messages:\n"
 grep 'odof.exception.ObjectNotFoundException' -B 1 $adminLog|head -6
-printf "\n\n"
+printf "\n--------------\n"
 printf "\nLast messages:\n"
 egrep 'odof.exception.ObjectNotFoundException' -B 1 $adminLog|tail -6
 printf "\n----------------------\n"
