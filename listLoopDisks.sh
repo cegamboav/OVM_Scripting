0f8594841e28c409574346cb52fe8cdb77886d15
djcerdas <d2words@gmail.com>
2017-12-04 18:05:24 -0600

    Initial commit
    - Make a script based on the line below line, to obtain the correspond loop device.
    # awk -F"'" '/disk/ { for (i = 2; i <= NF; i=i+2) print $i }' $(find /OVS -wholename '*VirtualMachines/*/vm.cfg' -exec grep -Hi 'simple_name' {} \; | grep "ID_of_the_VM" |cut -d':' -f1 )|cut -d"," -f1,2

---
+++ listLoopDisks.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Name        	: listLoopDisks.sh
+# Author      	: carlos.gamboa and david.cerdas
+# Version     	: 1
+# Copyright   	: GPLv2
+# Description	: This script lists the disks and its correspond loop , 
+# 				  based on the vm.cfg file of the running VMs in Dom0, OVM.	
+# usage			:		
+
+clear
+echo "Server Name:"
+hostname
+echo "----------------------------------------------------------------------------------------"
+
+for i in `xm list|awk '{print $1}'|egrep -v 'Name|Domain-0'`;
+        do
+        for j in `find /OVS/Repositories/ -name $i`;
+                do
+                echo VM "ID and Name:"
+                echo $i
+                cat $j/vm.cfg|egrep 'OVM_simple_name'
+                echo
+                cat $j/vm.cfg|egrep 'disk' > /tmp/disk.tmp
+
+                echo "Loops:"
+                echo
+                echo > /tmp/disk2.tmp
+                awk -F"'" '/disk/ { for (i = 2; i <= NF; i=i+2) print $i }' /tmp/disk.tmp|cut -d':' -f2|cut -d"," -f1 >> /tmp/disk2.tmp
+                for h in `cat /tmp/disk2.tmp`
+                        do
+                        echo "device:  "$h
+                        echo "Loop:    "`losetup -j $h`
+                        echo "--------------------"
+                done
+        done
+done
+echo "----------------------------------------------------------------------------------------"
