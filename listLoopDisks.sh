825f24438501d83aa07f256334d0f6fdf0ae190d
cegamboav <cegamboav@gmail.com>
2018-02-15 16:20:58 -0600

    Update listLoopDisks.sh

--- listLoopDisks.sh
+++ listLoopDisks.sh
@@ -1,37 +1,67 @@
 #!/bin/bash
 
-# Name        	: listLoopDisks.sh
-# Author      	: carlos.gamboa, david.cerdas and luis.alvarez
-# Version     	: 1
-# Copyright   	: GPLv2
-# Description	: This script lists the disks and its correspond loop , 
-# 		   based on the vm.cfg file of the running VMs in Dom0, OVM.	
-# Usage		: ./listLoopDisks.sh 	
+# Name          : listLoopDisks.sh
+# Author        : carlos.gamboa, david.cerdas and luis.alvarez
+# Version       : 1.0
+# Copyright     : GPLv2
+# Description   : This script lists the disks and its correspond loop ,
+#                  based on the vm.cfg file of the running VMs in Dom0, OVM.
+# Usage         : ./listLoopDisks.sh -<option> <id>
 
-clear
-echo "Server Name:"
-hostname
-echo "----------------------------------------------------------------------------------------"
+parameter=$1
 
-for i in `xm list|awk '{print $1}'|egrep -v 'Name|Domain-0'`;
-        do
-        for j in `find /OVS/Repositories/ -name $i`;
-                do
-                echo "VM ID and Name:"
-                echo $i
-                cat $j/vm.cfg|egrep 'OVM_simple_name'
-                echo
-                cat $j/vm.cfg|egrep 'disk' > /tmp/disk.tmp
-                echo "Loops:"
-                echo
-                echo > /tmp/disk2.tmp
-                awk -F"'" '/disk/ { for (i = 2; i <= NF; i=i+2) print $i }' /tmp/disk.tmp|cut -d':' -f2|cut -d"," -f1 >> /tmp/disk2.tmp
-                for h in `cat /tmp/disk2.tmp|grep -v cdrom`
-                        do
-                        echo "device:  "$h
-                        echo "Loop:    "`losetup -j $h`
-                        echo "--------------------"
-                done
-        done
-done
-echo "----------------------------------------------------------------------------------------"
+case_null(){
+	echo "Usage:"
+    echo -e " listLoopDisks.sh <desire> <VM_ID> \n\n"	
+	echo " Desire                                give info:"
+    echo " listLoopDisks.sh -a         |         Shows info of all running VMs"
+    echo " listLoopDisks.sh -i <VM_ID> |         Specify a VM ID"
+    echo ""
+    exit 1
+}
+
+header(){
+	clear
+	echo "Server Name:"
+	hostname
+	echo "----------------------------------------------------------------------------------------"
+}
+
+run_all(){
+	if [ $1 -eq 0 ];then
+		xmli="$(xm list|egrep -v 'Name|Domain-0'|awk '{print $1}' 2>&1)"
+	else
+		xmli="$(xm list $2|egrep -v 'Name|Domain-0'|awk '{print $1}' 2>&1)"
+	fi
+	
+	for i in $(echo "$xmli"); do
+		for j in `find /OVS/Repositories/ -name $i`; do
+			echo "VM Name				ID"
+			echo `cat $j/vm.cfg|egrep 'OVM_simple_name'|cut -d"=" -f2|cut -d"'" -f2`"			"$i
+			echo
+			disk="$(cat $j/vm.cfg|egrep 'disk')"
+			echo "Device						Repository			Loop"
+			disk2="$(echo $disk|awk -F"'" '/disk/ { for (i = 2; i <= NF; i=i+2) print $i }'|cut -d':' -f2|cut -d"," -f1)"
+			for h in $(echo "$disk2"|grep -v cdrom); do
+				repo="$(echo $h|cut -d'/' -f4)"
+				echo ""`echo $h|cut -d'/' -f6`"		"`grep OVS_REPO_ALIAS /OVS/Repositories/$repo/.ovsrepo|cut -d"=" -f2`"			"`losetup -j $h|awk '{print $1}'|cut -d':' -f1`
+			done			
+			echo ""
+		done
+	done
+}
+
+case $parameter in
+
+	-a|-A)
+		header
+		run_all 0
+		;;
+	-i|-I)
+		header
+		run_all 1 $2
+		;;
+	*)
+		case_null
+		;;
+esac
