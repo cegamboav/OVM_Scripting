65bfa4091371c49eb94355e50d7679d20d941273
djcerdas <d2words@gmail.com>
2018-02-23 11:51:37 -0600

    update egrep lines and add another author
    - Not necessarily all complete backups are good backups.
    - egrep "Not enough disk space" in the logs.
    - add david.cerdas to the contributors.

--- ovmDBCheck.sh
+++ ovmDBCheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Name        : ovmDBCheck.sh
-# Author      : carlos.gamboa
+# Author      : carlos.gamboa , david.cerdas  
 # Version     : 1
 # Copyright   : GPLv2
 # Description : Oracle OVM Manager/OVS script to check the OVM Manager DB for known inconsistencies
@@ -10,7 +10,7 @@
 adminLog=`find /u01 -name AdminServer.log`
 echo $adminLog
 printf "\nChecking the backups:\n----------------------\n"
-printf "Last good backups:\n"
+printf "Last complete backups:\n"
 grep backup $adminLog|grep complete|tail -5
 printf "\n----------------------\n"
 printf "Inconsistencies:\n"
@@ -23,3 +23,4 @@ printf "\n--------------\n"
 printf "\nLast messages:\n"
 egrep 'odof.exception.ObjectNotFoundException' -B 1 $adminLog|tail -6
 printf "\n----------------------\n"
+egrep "Not enough disk space" $adminLog|head -n2
