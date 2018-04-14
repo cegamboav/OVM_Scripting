04cc6513851df6bfdc6438d56442c01763a65145
djcerdas <d2words@gmail.com>
2018-04-13 20:29:36 -0600

    incorporate mysqldump and tar to obtain more data,
    - use /var/lib/mysql-files if the directory exists
    - use mysqldump to obtain the schema of the table and the OVM DB with all its data
    - use tar to create a compressed tarball with all the data

--- queryOVMTable.sh
+++ queryOVMTable.sh
@@ -2,48 +2,69 @@
 
 # Name        : queryOVMTable.sh
 # Author      : david.cerdas  
-# Version     : 0.1
+# Version     : 1.0
 # Copyright   : GPLv2
-# Description : This script check the current status of the OVM_STATISTIC table,
-#				and produces 3 output files: 1 with the OVM_STATISTIC characteristics,
-# 				and 2 queries to this table, with 30 seconds of difference.
-# Usage		: ./queryOVM_STATISTIC.sh <Password of the OVM Manager>
+# Description : This script checks the current status of a
+#				table in the OVM Manager MySQL DataBase.
+# Usage		: ./queryOVMTable.sh <Password of the OVM Manager>
 
-
+dir="/tmp"
 password=$1
+
+function mysql_files(){
+	if [ -d /var/lib/mysql-files ];then
+		chmod 750 /var/lib/mysql-files/
+	fi
+}
 
 # if there is not valid password given, request the customer to provide one
 function case_null(){
 	clear
-	echo -e "Please run queryOVM_STATISTIC.sh again, but provide a valid password\n"
-	echo "./queryOVM_STATISTIC.sh <Password of the OVM Manager>"
+	echo -e "Please run queryOVMTable.sh again, but provide a valid password\n"
+	echo "./queryOVMTable.sh <Password of the OVM Manager>"
     exit 1
 }
 
-
 if [ "${password:-null}" != "null" ];then
-	
-	rm -fr /tmp/dataOVM_STATISTIC*.csv
-	clear
-	echo "Start collecting the data, this might take few minutes please wait ..."
+
+	# use /var/lib/mysql-files/ to save the output files if this dir exists
+	if [ -d /var/lib/mysql-files ];then
+		dir="/var/lib/mysql-files"
+		chmod 757 /var/lib/mysql-files/
+	fi
+
+	rm -fr $dir/OVM_STATISTIC_QUERY*
+	clear&&echo -e "Start collecting the data, this might take few minutes please wait ...\n\n"
+	# describe the table and export the queries to OVM_STATISTIC_QUERY*.csv files
 	mysql -u root -p$password -S /u01/app/oracle/mysql/data/mysqld.sock -e "
 	USE ovs;
 	SHOW TABLES IN ovs LIKE 'OVM_STATISTIC';
-	DESCRIBE OVM_STATISTIC;SHOW TABLE STATUS IN ovs LIKE 'OVM_STATISTIC';
-	SELECT * FROM OVM_STATISTIC INTO OUTFILE '/tmp/dataOVM_STATISTIC1.csv' CHARACTER SET utf8  FIELDS TERMINATED BY ',' ENCLOSED BY '|' LINES TERMINATED BY '\r\n';
+	DESCRIBE OVM_STATISTIC;
+	SHOW TABLE STATUS IN ovs LIKE 'OVM_STATISTIC';
+	SELECT * FROM OVM_STATISTIC INTO OUTFILE '$dir/OVM_STATISTIC_QUERY1.csv' CHARACTER SET utf8  FIELDS TERMINATED BY ',' ENCLOSED BY '|' LINES TERMINATED BY '\r\n';
 	SELECT NOW();
 	SELECT COUNT(*) FROM OVM_STATISTIC;
-	SELECT SLEEP(30);
-	SELECT * FROM OVM_STATISTIC INTO OUTFILE '/tmp/dataOVM_STATISTIC2.csv' CHARACTER SET utf8 FIELDS TERMINATED BY ',' ENCLOSED BY '|' LINES TERMINATED BY '\r\n';
+	SELECT SLEEP(15);
+	SELECT * FROM OVM_STATISTIC INTO OUTFILE '$dir/OVM_STATISTIC_QUERY2.csv' CHARACTER SET utf8 FIELDS TERMINATED BY ',' ENCLOSED BY '|' LINES TERMINATED BY '\r\n';
 	SELECT NOW();
 	SELECT COUNT(*) FROM OVM_STATISTIC;
-	QUIT" &>/tmp/dataOVM_STATISTIC.status
-    
+	QUIT"
+	# Export the schema of the OVM_STATISTIC table, without any data.
+	mysqldump -u root -p$password -S /u01/app/oracle/mysql/data/mysqld.sock --no-data ovs OVM_STATISTIC 2>/dev/null|egrep -v "40*|--|^$" > $dir/OVM_STATISTIC_QUERY_TABLE.sql
+    # Export the current ovs DB to a compressed .sql file
+	mysqldump -u root -p$password ovs 2>/dev/null| gzip > $dir/OVM_STATISTIC_QUERY_OVS_DB.sql.gz
+	
 	if [ "$?" -eq "0" ];then
-		clear;echo "Done, thanks. Please provide the below files:"
-		ls -1 /tmp/*OVM_STATISTIC*
+		mysql_files
+		echo -e "\n-----------------------------------------------------------------------------"
+		echo -e "Done, thanks.\nPlease provide the output in this terminal with the below file:"
+		echo -e "-----------------------------------------------------------------------------\n"
+		tar zcf /tmp/OVM_STATISTIC.`date +"%d-%m-%Y"`.tar.gz $dir/OVM_STATISTIC_QUERY* 2>/dev/null
+		rm -fr $dir/OVM_STATISTIC_QUERY*
+		ls -1 /tmp/OVM_STATISTIC.`date +"%d-%m-%Y"`.tar.gz
 	else
-		echo "It was not possible to get the data"
+		mysql_files
+		echo "It was not possible to collect the data"
 	fi
 	
 else
